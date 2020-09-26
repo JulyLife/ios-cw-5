07a27653e9fa5d5a689b9efadc8fa54b42ba6a61
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-09-26 16:16:04 +0300

    first app

---
+++ ios-cw-5-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-cw-5-1
+//
+//  Created by ghanima on 9/26/20.
+//  Copyright © 2020 ghanima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
